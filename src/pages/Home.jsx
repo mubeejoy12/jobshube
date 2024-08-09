e0d5a4dc4f6f1a37500a86bbf62a188e7ce45d8f
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-10 00:27:13 +0100

    imported  the  navbar  component in the Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Box } from "@mui/material";
+
+const Home = () => {
+  return (
+    <Box>
+      <Navbar />
+    </Box>
+  );
+};
+
+export default Home;
