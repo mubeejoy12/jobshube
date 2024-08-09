c8e0bd4dec4bd9e2761838a6d840e76e27264539
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-10 00:39:34 +0100

    setting the tailwindcss to function and work perfectly

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
-
-
+import { Box } from "@mui/material";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      {/* <Box>
+        <Navbar />
+      </Box> */}
+    </>
+  );
 }
 
 export default App;
