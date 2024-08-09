87f7df1db61c4b22be55f91cba255d78c67c5760
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-10 00:29:33 +0100

    imported come component from mui, uisng useState for user frriendly environment, and imported react  Router  link

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
