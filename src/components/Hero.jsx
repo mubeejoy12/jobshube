2e3f0b07f4f37c9214538c7a7db399ed6a025bb2
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-11 01:50:19 +0100

    adding image to the hero  page and  adding animation for user friendly, with  some responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,3 +21,27 @@
           },
         }}
       >
+        <Box
+          sx={{
+            flex: 1,
+            order: { xs: 1, md: 2 },
+            alignItems: "center",
+            width: "100%",
+            maxWidth: { xs: "80%", sm: "70%", md: "100%" },
+            margin: "0 auto",
+          }}
+        >
+          <Box
+            component="img"
+            src={jobhubImg}
+            alt="Jobshub Image"
+            sx={{
+              width: "100%",
+              height: "auto",
+              maxWidth: { xs: "100%", md: "95%" },
+              animation:
+                "upDown 2s ease-in-out infinite alternate-reverse both",
+            }}
+          />
+        </Box>
+      </Box>
