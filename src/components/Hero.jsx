622a46501d22f7af1968ea67cd23ce1539868dee
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-11 01:47:05 +0100

    seetting the container and  center it to  the middle

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+    <Box
+      sx={{
+        background: "#36517e",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "20px",
+      }}
+    >
