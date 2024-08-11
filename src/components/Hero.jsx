a65150ccdcb56555e4013f4660c39c4748e76279
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-11 01:51:36 +0100

    Hero component consist the front page of the site with the main aim of the side, some text and images desmostrate whats its entails

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,11 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import { Box, Button, Stack } from "@mui/material";
+import jobhubImg from "../assets/jobhub-image.png";
+
+const Hero = () => {
+  return (
+    // container
     <Box
       sx={{
         background: "#36517e",
@@ -21,6 +29,57 @@
           },
         }}
       >
+        <Stack
+          sx={{
+            flex: 1,
+            order: { xs: 2, md: 1 },
+            alignItems: "flex-start",
+            spacing: 2,
+            textAlign: { xs: "center", md: "left" },
+            maxWidth: { xs: "100%", md: "50%" },
+          }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              fontSize: { xs: "32px", md: "45px" },
+              fontWeight: "700",
+              lineHeight: { xs: "40px", md: "56px" },
+              color: "#fff",
+              width: "100%",
+            }}
+          >
+            Welcome to Jobshub where we link talents with exceptional jobs
+          </Typography>
+          <Typography
+            variant="h2"
+            sx={{
+              fontSize: { xs: "20px", md: "24px" },
+              color: "rgba(255, 255, 255, 0.6)",
+              width: "100%",
+              paddingBottom: "40px",
+            }}
+          >
+            Elevate your workforce with our seamless remote job placement
+            opportunities
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              fontSize: "16px",
+              letterSpacing: "1px",
+              display: "inline-block",
+              borderRadius: "50px",
+              transition: "0.5s",
+              padding: "10px 24px 11px 24px",
+              background: "#47b2e4",
+              marginTop: { xs: "16px", md: "24px" }, 
+            }}
+          >
+            Apply Now
+          </Button>
+        </Stack>
         <Box
           sx={{
             flex: 1,
@@ -45,3 +104,8 @@
           />
         </Box>
       </Box>
+    </Box>
+  );
+};
+
+export default Hero;
