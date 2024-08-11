8d3b036fb929f90e619274da4125eba2fa2c1f29
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-11 01:53:48 +0100

    adding Hero component to Home page and added a box to space the navbar from hero component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import { Box } from "@mui/material";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <Box>
       <Navbar />
+      <Box sx={{ height: '64px', background: "#36517e" }} /> {/* Spacer for Navbar */}
+      <Hero/>
     </Box>
   );
 };
