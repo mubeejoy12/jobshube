18a73ecc3685b56f5ba53926ac20103c79932042
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-12 15:36:35 +0100

    imported images and used in grid system

---
+++ src/components/Clients.jsx
@@ -0,0 +1,6 @@
+import client1 from "../assets/client-1.png";
+import client2 from "../assets/client-2.png";
+import client3 from "../assets/client-3.png";
+import client4 from "../assets/client-4.png";
+import client5 from "../assets/client-5.png";
+import client6 from "../assets/client-6.png";
