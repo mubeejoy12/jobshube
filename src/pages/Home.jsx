a3a392bf0775d493ec7626b53798f58477f535b9
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-12 15:34:35 +0100

    imported clients component  to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { Box } from "@mui/material";
 import Hero from "../components/Hero";
+import Clients from "../components/Clients";
 
 const Home = () => {
   return (
     <Box>
       <Navbar />
-      <Box sx={{ height: '64px', background: "#36517e" }} /> {/* Spacer for Navbar */}
-      <Hero/>
+      <Box sx={{ height: "64px", background: "#36517e" }} />{" "}
+      {/* Spacer for Navbar */}
+      <Hero />
+      <Clients />
     </Box>
   );
 };
