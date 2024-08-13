63865b2c616ad9f0f2a93c472a85d0352fd6969c
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-13 23:08:19 +0100

    using props for the Header component o it can be reusable

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+const Header = ({ text }) => {
+  return (
+    <Typography
+      variant="h3"
+      sx={{
+        fontWeight: "bold",
+        color: "#36517e",
+        letterSpacing: "0.1em",
+        fontSize: "32px",
+        textAlign: "center",
+      }}
+    >
+      {text}
+    
+    </Typography>
+  );
+};
+
+export default Header;
