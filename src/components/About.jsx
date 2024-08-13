03e4528a6035d64d9292a30c58fb8b229e5257c1
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-13 23:13:56 +0100

    mapped through the arrays of sentenses

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,30 @@
 import DoneAllIcon from "@mui/icons-material/DoneAll";
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+          <Stack>
+            {[
+              "Jobshub offers a meaningful number of Connections to talents.",
+              "We are passionate about changing the Workforce Landscape.",
+              "Jobshub is positioned as a dedicated Job Company",
+            ].map((text, index) => (
+              <Stack
+                key={index}
+                display={"flex"}
+                sx={{ flexDirection: "row", alignItems: "center", mb: 1 }}
+              >
+                <DoneAllIcon
+                  sx={{
+                    color: "#47b2e4",
+                    mr: 1,
+                    fontSize: isMobile ? "1rem" : "1.5rem",
+                  }}
+                />
+                <Typography
+                  variant={isMobile ? "body2" : "subtitle1"}
+                  color="initial"
+                >
+                  {text}
+                </Typography>
+              </Stack>
+            ))}
+          </Stack>
