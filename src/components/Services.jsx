4f41691a7f61f0bf17a1bc5ddf88460732dbc4c0
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-14 01:43:00 +0100

    array of cardData object

---
+++ src/components/Services.jsx
@@ -0,0 +1,26 @@
+const cardData = [
+  {
+    icon: <SportsBasketballIcon sx={{ fontSize: "40px", color: "#47b2e4" }} />,
+    title: "Personalized Matchmaking",
+    description:
+      "Tailoring job placements to match the unique needs of both employers and professionals.",
+  },
+  {
+    icon: <DescriptionIcon sx={{ fontSize: "40px", color: "#47b2e4" }} />,
+    title: "Efficient Recruitment",
+    description:
+      "Streamlining the hiring process for quick, effective, and resource-efficient results.",
+  },
+  {
+    icon: <LayersIcon sx={{ fontSize: "40px", color: "#47b2e4" }} />,
+    title: "Diverse Talent Network",
+    description:
+      "Access a diverse pool of skilled candidates, contributing to a dynamic and inclusive workforce.",
+  },
+  {
+    icon: <SpeedIcon sx={{ fontSize: "40px", color: "#47b2e4" }} />,
+    title: "Ongoing Support",
+    description:
+      "Beyond placements, Jobshub is dedicated to providing continuous support for long-term success.",
+  },
+];
