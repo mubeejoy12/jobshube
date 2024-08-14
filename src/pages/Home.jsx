a68841a2729c7d86f5c04debdf39027491571978
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-08-14 01:41:31 +0100

    modifies home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,22 @@ import { Box } from "@mui/material";
 import Hero from "../components/Hero";
 import Clients from "../components/Clients";
 import About from "../components/About";
+import Services from "../components/Services";
+import Action from "../components/Action";
 
 const Home = () => {
   return (
     <Box>
       <Navbar />
-      <Box sx={{ height: "64px", background: "#36517e" }} />{" "}
+      <Box
+        sx={{ height: "64px", background: "#36517e",  }}
+      />{" "}
       {/* Spacer for Navbar */}
       <Hero />
       <Clients />
       <About />
+      <Services />
+      <Action/>
     </Box>
   );
 };
