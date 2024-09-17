08dd1582a2a00c3b833445fba345a1e2cc7f5e9a
mubarak <abubakrobasanjo@gmail.con>
2024-09-17 07:27:40 +0100

    created array of object for teammembers

---
+++ src/components/Team.jsx
@@ -0,0 +1,29 @@
+  const teamMembers = [
+    {
+      image: Team1,
+      name: "Walter White",
+      position: "Chief Executive Officer",
+      description:
+        "Explicabo voluptatem mollitia et repellat qui dolorum quasi",
+    },
+    {
+      image: Team2,
+      name: "Sarah Jhonson",
+      position: "Product Manager",
+      description:
+        "Aut maiores voluptates amet et quis praesentium qui senda para",
+    },
+    {
+      image: Team3,
+      name: "William Anderson",
+      position: "CTO",
+      description:
+        "Quisquam facilis cum velit laborum corrupti fuga rerum quia",
+    },
+    {
+      image: Team4,
+      name: "Amanda Jepson",
+      position: "Accountant",
+      description:
+        "Dolorum tempora officiis odit laborum officiis et et accusamus",
+    },
