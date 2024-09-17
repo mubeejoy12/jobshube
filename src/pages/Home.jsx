c1abf6cffbb0c0e386a98959feb3db2e17f8caf6
mubarak <abubakrobasanjo@gmail.con>
2024-09-17 07:35:08 +0100

    imported Team conponent

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import Navbar from "../components/Navbar";
 import { Box } from "@mui/material";
 import Hero from "../components/Hero";
@@ -6,6 +6,7 @@ import Clients from "../components/Clients";
 import About from "../components/About";
 import Services from "../components/Services";
 import Action from "../components/Action";
+import Team from "../components/Team";
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
       <About />
       <Services />
       <Action/>
+      <Team/>
     </Box>
   );
 };
