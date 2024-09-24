03fbfbe5e41239f31c76f8b1a8a1fadd51bda583
mubeejoy12 <83483923+mubeejoy12@users.noreply.github.com>
2024-09-24 13:13:58 +0100

    Commented out unused code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { Box } from "@mui/material";
+// import { Box } from "@mui/material";
 import "./App.css";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route, BrowserRouter } from "react-router-dom";
+// import Applyow from "./components/Applyow";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/team" element={<Team />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/apply" element={<ApplyForm />} /> 
+      <Route path="*" element={<NotFound />} /> 
+    </Routes>
+  </BrowserRouter> */}
+
+      {/* jjjjkkkkl */}
+      <Home />
+      {/* <Routes>
         <Route path="/" element={<Home />} />
-      </Routes>
+        <Route path="/applynow" element={<Applyow />} />
+      </Routes> */}
       {/* <Box>
         <Navbar />
       </Box> */}
